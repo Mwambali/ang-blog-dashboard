a819f6d34cc6c6a3aad7478ad1f674dfcb9f078f
Mwambali <ishimwesion@gmail.com>
2023-07-17 16:40:38 +0200

    dashboard-backend setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from '@angular/fire';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment)
   ],
   providers: [],
   bootstrap: [AppComponent]
