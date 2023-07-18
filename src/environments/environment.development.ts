bf3cda82903d709f68df9e92031d17313f6f30ec
Mwambali <ishimwesion@gmail.com>
2023-07-18 19:00:55 +0200

    add environments directory

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-blog-54a19',
+    appId: '1:47597682279:web:a34fbfad54eec04ec55ec2',
+    storageBucket: 'angular-blog-54a19.appspot.com',
+    apiKey: 'AIzaSyD3O-6mVAL4TS8TxmcFET2Qv84xLfaVEIY',
+    authDomain: 'angular-blog-54a19.firebaseapp.com',
+    messagingSenderId: '47597682279',
+  },
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyD3O-6mVAL4TS8TxmcFET2Qv84xLfaVEIY",
+        authDomain: "angular-blog-54a19.firebaseapp.com",
+        projectId: "angular-blog-54a19",
+        storageBucket: "angular-blog-54a19.appspot.com",
+        messagingSenderId: "47597682279",
+        appId: "1:47597682279:web:a34fbfad54eec04ec55ec2"
+    }
+};
