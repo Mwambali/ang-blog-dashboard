6d75f378d975304b83a5b9fda70f1f5003051f90
Mwambali <ishimwesion@gmail.com>
2023-08-04 19:02:14 +0200

    add class

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,28 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms'; // Import NgForm type
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-  formData: any;
+  // formData: any;
+  // categoryArray: Array<object>
 
-  constructor(private afs: AngularFirestore) { }
+  // constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    // this.categoryService.loadData().subscribe(val => {
+    //   console.log(val);
+    //   this.categoryArray = val;
 
+    // })
   }
 
   onSubmit(formData: NgForm) { // Explicitly specify the type as NgForm
-    let categoryData = {
-      category: formData.value.category
-    }
-    this.afs.collection('categories').add(categoryData).then(docRef => {
-      console.log(docRef);
-    })
-      .catch(err => console.log(err))
+    // let categoryData = {
+    //   category: formData.value.category
+    // }
+    // this.afs.collection('categories').add(categoryData).then(docRef => {
+    //   console.log(docRef);
+    // })
+    //   .catch(err => console.log(err))
   }
 
 }
